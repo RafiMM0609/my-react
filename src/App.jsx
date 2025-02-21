68158baf9e9df94b341155a82cea3777ae58726d
RAFI MUHAMMAD MAHRUS <rafi.mahrus@sigma.co.id>
2025-02-21 08:48:37 +0700

    adjust hamburger icon

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,21 @@ import './App.css'
 
 function Navbar({bgColor="bg-emerald-100"}) {
   const [dropdown, setDropdown] = useState(false);
+  
   return (
     <nav className={`${dropdown ? "min-h-screen bg-stone-700" : `min-h-0 ${bgColor}`} transition-all duration-1000 ease-in-out fixed top-0 left-0 pl-6 pr-6 pt-4 w-full z-50`}>
       <div className="container mx-auto flex justify-between">
-        <div id="menu_icon" className={`items-center space-x-4 text-l`}>
-          <img src='../icon-menu.png' alt='Menu Icon' className='h-4 w-4' onClick={()=>setDropdown(!dropdown)}/>
+        <div 
+          id="menu_icon" 
+          className="flex flex-col justify-center items-center w-10 h-10 cursor-pointer space-y-1"
+          onClick={() => setDropdown(!dropdown)}
+        >
+          <span className={`static w-8 h-2 bg-black rounded transition-all duration-300 
+            ${dropdown ? "rotate-45 translate-y-2.5" : ""}`}
+          />
+          <span className={`static w-8 h-2 bg-black rounded transition-all duration-300 
+            ${dropdown ? "-rotate-45 -translate-y-1" : ""}`}
+          />
         </div>
         {dropdown && (
         <div className='flex flex-row bg-stone-700 pt-40'>
@@ -35,7 +45,7 @@ function Navbar({bgColor="bg-emerald-100"}) {
           </div>
         </div>
       )}
-        <p className={`${dropdown ? "invisible" : "visible"} text-xl font-bold`}>
+        <p className={`${dropdown ? "invisible" : "visible"} text-3xl font-bold`}>
           <span className='text-stone-600'>Lo</span>
           <span className='text-cyan-600'>Go</span>
         </p>
